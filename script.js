11bac583653a85d28943d6b6e6ca21191f60aaba
NavaneethaKrishna26 <159992380+NavaneethaKrishna26@users.noreply.github.com>
2024-10-06 10:20:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const menuIcon=document.querySelector('#menuicon');
+const navLinks =document.querySelector('.navlinks');
+
+menuIcon.onclick=()=>{
+    navLinks.classList.toggle('active');
+
+
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "20px";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
